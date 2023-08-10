d021380ae03cc8d9cadd7cb8ccf3f4de62155fd4
tmdduq500 <osy9907@gmail.com>
2023-08-10 20:19:33 +0900

    보표 이미지(easy) 추가

---
+++ deep_learning_sy/color_change.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+
+# 입력 폴더 경로 설정
+input_folder = r'D:\music-sheet-algoritm\deep_learning_sy\easy_staff_img'
+
+# 출력 폴더 경로 설정
+output_folder = r'D:\music-sheet-algoritm\deep_learning_sy\oo'
+
+# 입력 폴더 내 모든 이미지 파일 불러오기
+image_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.png') or f.lower().endswith('.jpg')]
+
+# 각 이미지 파일에 대해 작업 수행
+for image_file in image_files:
+    input_path = os.path.join(input_folder, image_file)
+    
+    # 이미지 불러오기
+    binary_sheet = cv2.imread(input_path, cv2.IMREAD_ANYCOLOR)
+    
+    # 색상 반전
+    colored_sheet = cv2.bitwise_not(binary_sheet)
+    
+    # 출력 파일 경로 설정
+    output_path = os.path.join(output_folder, image_file)
+    
+    # 이미지 저장
+    cv2.imwrite(output_path, colored_sheet)
--- deep_learning_sy/main_staves_cut.py
+++ deep_learning_sy/main_staves_cut.py
@@ -5,17 +5,11 @@ import numpy as np
 import functions as fs
 import modules as modules_sy
 
-#이미지 불러오기
-image=cv2.imread(r"D:\re-music-sheet-algoritm\sy\music_sheet_jpg\1.jpg", cv2.IMREAD_ANYCOLOR)
-
-#전처리 1. 보표 영역 추출 및 그 외 노이즈 제거
-masked_image = modules_sy.remove_noise(image)
-
 # 악보 이미지들이 들어있는 폴더 경로
-input_folder = r"D:\re-music-sheet-algoritm\sy\dr_sheet_jpg"
+input_folder = r"D:\music-sheet-algoritm\deep_learning_sy\easy_drum_jpg"
 
 # 보표 추출한 이미지들을 저장할 폴더 경로
-output_folder = r"D:\re-music-sheet-algoritm\sy\extracted_symbols"
+output_folder = r"D:\music-sheet-algoritm\deep_learning_sy\easy_staff_img"
 
 # 보표 추출 작업
 def extract_symbols_from_images(input_folder, output_folder):
