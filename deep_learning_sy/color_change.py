4f96206328a96615d281930c3d5b9b63ebb8888c
tmdduq500 <osy9907@gmail.com>
2023-08-11 15:39:29 +0900

    보표 이미지(음표1,2개) 추가

--- deep_learning_sy/color_change.py
+++ deep_learning_sy/color_change.py
@@ -2,10 +2,10 @@ import os
 import cv2
 
 # 입력 폴더 경로 설정
-input_folder = r'D:\music-sheet-algoritm\deep_learning_sy\easy_staff_img'
+input_folder = r'D:\music-sheet-algoritm\easy_staff_img'
 
 # 출력 폴더 경로 설정
-output_folder = r'D:\music-sheet-algoritm\deep_learning_sy\oo'
+output_folder = r'D:\music-sheet-algoritm\easy_staff_color'
 
 # 입력 폴더 내 모든 이미지 파일 불러오기
 image_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.png') or f.lower().endswith('.jpg')]
--- deep_learning_sy/main_staves_cut.py
+++ deep_learning_sy/main_staves_cut.py
@@ -6,10 +6,10 @@ import functions as fs
 import modules as modules_sy
 
 # 악보 이미지들이 들어있는 폴더 경로
-input_folder = r"D:\music-sheet-algoritm\deep_learning_sy\easy_drum_jpg"
+input_folder = r"D:\music-sheet-algoritm\easy_drum_img"
 
 # 보표 추출한 이미지들을 저장할 폴더 경로
-output_folder = r"D:\music-sheet-algoritm\deep_learning_sy\easy_staff_img"
+output_folder = r"D:\music-sheet-algoritm\easy_staff_img"
 
 # 보표 추출 작업
 def extract_symbols_from_images(input_folder, output_folder):
@@ -28,7 +28,7 @@ def extract_symbols_from_images(input_folder, output_folder):
 
             # 보표 추출한 이미지들을 순회하며 파일로 저장
             for i, symbol in enumerate(extracted_symbols):
-                symbol_filename = os.path.join(output_folder, f"{filename}_symbol_{i}.png")
+                symbol_filename = os.path.join(output_folder, f"{filename}_symbol_{i}.jpg")
                 cv2.imwrite(symbol_filename, symbol)
 
 # 악보 이미지들로부터 보표 추출 수행
