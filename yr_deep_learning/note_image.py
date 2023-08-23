732281da8909e682b92bcdbecb8218cdbff1dfae
yurim <yurim0412@inu.ac.kr>
2023-08-23 10:58:18 +0900

    음표 이미지 수집중

--- yr_deep_learning/note_image.py
+++ yr_deep_learning/note_image.py
@@ -1,9 +1,10 @@
 from PIL import Image
 import os
+import random
 
 # 입력 및 출력 폴더 경로 설정
-input_folder = r'C:\music-sheet-algorithm\yr_deep_learning\easy_staff_color'
-output_folder = r'C:\music-sheet-algorithm\yr_deep_learning\notes'
+input_folder = r'C:\music-sheet-algoritm\yr_deep_learning\easy_staff_color'
+output_folder = r'C:\music-sheet-algoritm\yr_deep_learning\notes'
 
 # 입력 폴더 내의 모든 이미지 파일을 가져옴
 image_files = [f for f in os.listdir(input_folder) if f.endswith('.jpg')]
@@ -12,13 +13,16 @@ for image_file in image_files:
     image_path = os.path.join(input_folder, image_file)
     image = Image.open(image_path)
 
-    # 이미지 처리 작업을 위한 코드 추가
-    # 예시: 이미지를 그레이스케일로 변환하여 음표 객체 검출
-    gray_image = image.convert("L")  # 그레이스케일 변환
-    # 이미지 처리 및 음표 객체 검출 작업을 추가해야 함
+    # 음표 객체 검출 및 잘라내기 작업
+    # 여기에 실제 이미지 처리 코드를 추가해야 함
 
+    # 임의의 위치와 크기로 음표 객체의 좌표 정보 생성 (예시)
+    x = random.randint(0, image.width - 50)  # 임의의 x 좌표
+    y = random.randint(0, image.height - 50)  # 임의의 y 좌표
+    width = 50  # 임의의 너비
+    height = 50  # 임의의 높이
+    
     # 검출된 음표 객체의 좌표 정보를 사용하여 잘라내기
-    # 예시: 임의의 좌표와 크기로 잘라냄
     note_bbox = (x, y, x + width, y + height)
     note_image = image.crop(note_bbox)
 
